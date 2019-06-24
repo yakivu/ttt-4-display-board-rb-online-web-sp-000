6a94c02fd6b0f4e30ffa77857f55b50d44238d53
Adam Walker <adamw207@yahoo.com>
2019-06-24 20:22:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,26 +1,29 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+end
+def display_board(board)
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+end
+def display_board(board)
 board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+end 
 board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
