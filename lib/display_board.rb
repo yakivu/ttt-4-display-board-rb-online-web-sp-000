b1996ce554001a9bbb58f734c4a1ea82ff47b263
Adam Walker <adamw207@yahoo.com>
2019-06-24 16:54:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,68 @@
-# Define display_board that accepts a board and prints
+board = [" "," "," "," "," "," "," "," "," "]# Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = ["0", " ", " ", " ", " ", " ", " ", " ", " "]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = [" ", " ", " ", " ", " ", " ", "0", "0", "0"]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+puts "#{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+ 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
