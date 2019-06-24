aa22c4cd073770fc06139f6799cf6d2b9f1f8a30
Adam Walker <adamw207@yahoo.com>
2019-06-24 20:57:01 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -74,7 +74,7 @@ puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
  end
  def display_board(board)
-board = ["k","k","k", "k ", "k ", "k ", "k  ", "k  ", "k ", "k "]
+board = ["O","O","O", "O ", "O ", "O ", "O  ", "O  ", "O ", "O "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
