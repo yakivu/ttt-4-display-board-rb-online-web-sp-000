9136e202b4a27d02c330ee58347a90f73bb44e78
Adam Walker <adamw207@yahoo.com>
2019-06-24 20:25:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -24,57 +24,67 @@ puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
 end 
+def display_board(board)
 board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+ end
+ def display_board(board)
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+ end
+ def display_board(board)
 board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+end
+def display_board(board)
 board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+end
+def display_board(board)
 board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+ end
+ def display_board
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
- 
+ end
+ def display_board(board)
 board = ["X","X","X", "X ", "X ", "X", "X ", "X ", "X ", "X "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
+end
+def display_board(board)
   board = ["O","O","O", "O", "O", "O", "O", "O ", "O ", "O "]
 puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts "#{board[6]} | #{board[7]} | #{board[8]} "
+end
