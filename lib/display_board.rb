ab2f766cf216cb98c512885de4de748d7a5f2826
Adam Walker <adamw207@yahoo.com>
2019-06-24 21:08:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-player = ["X" , "O"]
+player = "X" , "O"
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
